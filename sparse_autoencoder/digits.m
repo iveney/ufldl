87034ae7f039b1dc2e43be433750ee5d92e32805
Zigang Xiao <iveney@gmail.com>
2014-04-27 16:43:47 -0500

    load from appropriate path

--- sparse_autoencoder/digits.m
+++ sparse_autoencoder/digits.m
@@ -1,7 +1,7 @@
 addpath mnistHelper
 
-images = loadMNISTImages('train-images.idx3-ubyte');
-labels = loadMNISTLabels('train-labels.idx1-ubyte');
+images = loadMNISTImages('../mnist/train-images.idx3-ubyte');
+labels = loadMNISTLabels('../mnist/train-labels.idx1-ubyte');
  
 % We are using display_network from the autoencoder code
 display_network(images(:,1:100)); % Show the first 100 images
